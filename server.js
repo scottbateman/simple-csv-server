c50621c643411f3d3d8fe252a3a869f464022e50
Scott Bateman <scottb@unb.ca>
2020-05-28 18:47:59 -0300

    Merged changes from scary-server, updated readme... tried to delete database.js again

--- server.js
+++ server.js
@@ -1,69 +1,84 @@
 var express = require("express");
 var app = express();
-var sqlinjection = require('sql-injection');
-app.use(sqlinjection);
+var bodyparser = require('body-parser');
+var fs = require('fs');
+
+app.use(bodyparser.text())
 
 const config = require('./config');
 const sqlite = require('sqlite3').verbose();
 
-//connect or create db
-var db = new sqlite3.Database(config.DB_NAME, sqlite3.READ_WRITE, (err) => {
-    if (err){
-        //if can't open for READ_WRITE try create
-        var db = new sqlite3.Database(config.DB_NAME, (err) => {
-            if (err){
-                console.log(`Cannot connect to db: ${config.DB_NAME}`);
-                process.exit();
-            }
-            //create tables
-            else
-            {
-                //create database creation statements
-                const DB_CREATION_STATEMENTS = [];
-                config.tables.forEach(table => {
-                    let create_table = `CREATE ${table.table_name} \(${table.join()}\)`;
-                    db.exec(create_table, (err, row) => {
-                        if (err){
-                            console.error('error creating table:'+ err);   
-                        }
-                        else
-                        {
-                            console.log('created table: '+row);
-                        }
-                    });    
-                });
 
-            }
-        }
+let table_create = false;
+
+//test to see if db exists
+try{
+    if (fs.existsSync(config.DB_NAME))
+    {
+        console.log(`${config.DB_NAME} found.`);
+    }
+    else{
+        console.log(`${config.DB_NAME} NOT found. Will create it and try to create tables.`);
+        table_create = true;
+    }
+} catch(err){
+    console.log(`${config.DB_NAME} NOT found. Will create it and try to create tables.`);
+    table_create = true;
+}
+
+
+//connect or create db
+var db = new sqlite.Database(config.DB_NAME, (err) => {
+    if (err){
+        console.log(`Cannot connect to db: ${config.DB_NAME}`);
+        process.exit();
+    }
+    //create tables
+    else if (table_create){
+        console.log("CREATING TABLES...");
+        //create database creation statements
+        config.tables.forEach(table=> {
+            let create_table = `CREATE TABLE ${table.table_name} \(${table.columns.join()}\)`;
+            console.log(create_table);
+            db.exec(create_table, (err, row) => {
+                if (err){
+                    console.error('error creating table:'+ err);   
+                }
+                else{
+                    console.log('created table');
+                }
+            });    
+        });
     }
     console.log(`Connected to db: ${config.DB_NAME}`);
 });
 
-
 //start server
-app.listen(HTTP_PORT, () => {
-    console.log("simple_csv_server running on port ${config.HTTP_PORT}");
+var http_server = app.listen(config.HTTP_PORT, () => {
+    console.log(`simple_csv_server running on port ${config.HTTP_PORT}`);
 });
 
-
-
 //set up endpoints 
-for (let i = 0; i < config.tables.length(); i++){
+for (let i = 0; i < config.tables.length; i++){
     table = config.tables[i];
-    app.post('/' + table.table_name, function(req, res){
-        let insert = `insert into ${table.table_name} values \(${req.body}\);`        
-        db.exec(insert, (err, row)=>{
-            if (err){   
-                let error_msg = `error on query:
-                    ${insert}`;
-                console.error(error_msg);
-                res.status(500).send(erro_msg);
-            }
-            else{
-                res.sendStatus(200);
-            }
-        });
-    }(table));
+    (function(table){
+        app.post('/' + table.table_name, function(req, res){
+            let insert = `insert into ${table.table_name} values \(${req.body}\);`        
+            db.exec(insert, (err, row)=>{
+                if (err){   
+                    let error_msg = `error on query:
+                        ${insert}
+                    `;
+                    console.error(error_msg);
+                    console.error(err);
+                    res.status(500).send(error_msg);
+                }
+                else{
+                    res.sendStatus(200);
+                }
+            });
+        })
+    })(table);
 }
 
 //default response for any other request
@@ -72,8 +87,9 @@ app.use(function(req,res){
    res.status(404).send("Not found");
 });
 
-function clenaup(){
-    app.close();
+function cleanup(){
+    console.log("Closing simple_csv_server");
+    http_server.close();
     db.close();
     process.exit();
 }
